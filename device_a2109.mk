483e27b80ddc4a966723f6d729074d66dbdd590d
Jake Stanger <jakestanger@gmail.com>
2018-06-20 15:18:06 +0100

    Additions and changes for Halium

--- device_a2109.mk
+++ device_a2109.mk
@@ -123,3 +123,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# twrp
+PRODUCT_COPY_FILES += device/lenovo/a2109/twrp.fstab:recovery/root/etc/twrp.fstab
